baa2f6c7835c2db24c7f7b8b984dee5b85081937
satiricalguru <jatinjio1212@gmail.com>
2026-04-20 03:23:57 +0530

    Initial Commit

---
+++ Ai Nexus/Ai_NexusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ai_NexusApp.swift
+//  Ai Nexus
+//
+//  Created by Lalit Pandey on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Ai_NexusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
